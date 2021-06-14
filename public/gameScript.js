1d2e806763e292ba404db0b4a6bf5a1d2df31a97
User01name <02goto1bif@hft-stuttgart.de>
2021-06-14 21:51:04 +0200

    headers angepasst, css auskommentiert, chessboard bearbeitet -> testweise, index.js und script angepasst an Web Socket

--- public/gameScript.js
+++ public/gameScript.js
@@ -1,11 +1,41 @@
 $(document).ready(() => {
     console.log("DOM is ready!");
 
+   
+    //Websocket:
+    const socket = io();
+    var roomDiv = document.getElementById('room');
+
+    $('#roomForm').submit((event) => { //wenn der submit button des roomFormulars gedrückt wird ...
+        event.preventDefault();
+        socket.emit('room', $('#r').val()); // verschicke über den room Kanal eine Nachricht mit dem Inhalt des Textfeldes
+        roomDiv.innerHTML += `Room: ${$('#r').val()}`; // und Ändere den Raumnamen
+        $('#r').val('');
+      });
+
+      socket.on('room', function (msg) { //wenn man eine Nachricht von dem room kanal bekommt ...
+        roomDiv.innerHTML += `Room: ${msg}`;//ändere den Text    
+      });
+
+});
+
+var startPos = new Array(8);
+for(i=0; i<startPos.length; i++){
+    startPos[i] = new Array(8);
+}
+
+function createStartArray(){
+    for (i=0; i<startPos.length; i++){
+        startPos[1][i] = "Bauer";
+        startPos[6][i] = "Bauer";
+    }
+    return startPos;
+}
+
+function paint(){
     var chessboardDiv = document.getElementById('chessboard');
 
-    farbe = '#ffffff';
-
-    var startPos = creatStartArray();
+    farbe = '#ccbf8e';
 
     for (z=0;z<8;z++) {
        
@@ -18,8 +48,8 @@ $(document).ready(() => {
             row+="</td>";
             
             if(s<7){
-                if( farbe == '#000000' ){ farbe = '#ffffff'}
-                else{ farbe = '#000000';}
+                if( farbe == '#976344' ){ farbe = '#ccbf8e'}
+                else{ farbe = '#976344';}
             }
         }
        
@@ -27,19 +57,29 @@ $(document).ready(() => {
         chessboardDiv.innerHTML += row;
     }
 
-});
+}
 
-function creatStartArray(){
-    var startPos = new Array(8);
-    for(i=0; i<startPos.length; i++){
-        startPos[i] = new Array(8);
-    }
+function repaint(startPosRow, startPosCol, endPosRow, endPosCol, figur){
+    var rows = document.getElementById('chessboard').getElementsByTagName('tr');
+    var data = rows[startPosRow].getElementsByTagName('td');
+    data[startPosCol].innerHTML = "";
 
-    for (i=0; i<startPos.length; i++){
-        startPos[1][i] = "Bauer";
-        startPos[6][i] = "Bauer";
-    }
+    data = rows[endPosRow].getElementsByTagName('td');
+    data[endPosCol].innerHTML = figur;
+}
 
+function clickFun() {
+    //window.location = "/hostroom";
+    createStartArray();
+    paint();
 
-    return startPos;
+}
+
+function exampleTurn(){
+    startPos [1][1] = "";
+    startPos [3][1] = "Bauer";
+    repaint(1,1,3,1,"Bauer");
+    //      rows[i].addEventListener('click', function() {
+      //  alert(this.rowIndex + 1);
+    //});
 }
